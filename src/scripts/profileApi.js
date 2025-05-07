2f57a08b7921e006a4ee18ffb610d1b8395199ca
matheussferraz <matheussferraz2@gmail.com>
2025-05-07 10:48:48 -0300

    atualizando o fetch do arquivo profileApi para buscar na api do github

--- src/scripts/profileApi.js
+++ src/scripts/profileApi.js
@@ -1,6 +1,6 @@
 export async function fetchProfileData() {
-    // const url = 'https://raw.githubusercontent.com/matheussferraz/js-developer-portfolio/refs/heads/main/data/profile.json'
-    const url = '/services/profile.json'
+    const url = 'https://raw.githubusercontent.com/matheussferraz/professional-profile-mff/refs/heads/main/services/profile.json'
+    //const url = '/services/profile.json'
     const response = await fetch(url)
     return await response.json()
 }
